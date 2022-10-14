e8f6fd7a47cce75768768547fcba7df2205fb2f3
Baiysh <115449261+kydyrmyshovbaiysh@users.noreply.github.com>
2022-10-14 17:46:24 +0600

    add Kitty

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 import "./App.css";
+const comment = [
+  {
+    id: 1,
+    date: new Date().toLocaleDateString(),
+    text: "I hope you enjoy learning React!",
+    author: {
+      name: "Hello Kitty",
+      avatarUrl: "http://placekitten.com/g/62/62",
+    },
+  },
+  {
+    id: 2,
+    date: new Date().toLocaleDateString(),
+    text: "I hope you enjoy learning React!",
+    author: {
+      name: "Hello Kitty",
+      avatarUrl: "http://placekitten.com/g/64/64",
+    },
+  },
+  {
+    id: 3,
+    date: new Date().toLocaleDateString(),
+    text: "I hope you enjoy learning React!",
+    author: {
+      name: "Hello Kitty",
+      avatarUrl: "http://placekitten.com/g/64/64",
+    },
+  },
+];
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    
+    
+    {comment.map((item)=>{
+      return (
+        <div key={item.id} >
+          <div className="photo-title">
+            <img src={item.author.avatarUrl} alt="" />
+            <p>{item.author.name}</p>
+
+          </div>
+          <div className="text">
+            <p>{item.text}</p>
+            <span>{item.date}</span>
+          </div>
+        </div>
+      );
+    })}
+  </div>
 }
 
 export default App;
